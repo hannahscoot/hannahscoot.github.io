72aaa415d8891b9c323dd6d95b3218a3ca51d604
Hannah <hscott1279@outlook.com>
2022-01-24 23:51:44 +0000

    t

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ resetgame = function () {
             affectedkeyboardrowlist[i].onclick = clickonletter(affectedkeyboardrowlist[i].id);
         }
     }
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 6; i++) {
         for (j=0; j < 5; j++) {
             document.getElementById(i.toString()+j.toString()).innerHTML = "";
         }
